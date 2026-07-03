8d1f2a7505b0b320a3e653046bf42445b86ac0d1
examforge99 <examforge99@gmail.com>
2026-07-03 23:16:13 +0100

    Add PolygonEditor component to Home page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import PolygonEditor from "@/components/PolygonEditor";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black">
+      <PolygonEditor />
+    </main>
+  );
+}
